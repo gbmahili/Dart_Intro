f33f473282d1f07544fc766a98b931f53c1da79d
Baraka Mahili <georgesmahili@gmail.com>
2019-05-11 23:01:04 -0400

    Adding classes and methods and constuctors

--- game_of_cards.dart
+++ game_of_cards.dart
@@ -1,14 +1,43 @@
+
 main() {
-  
+  var deck = new Deck();
+  deck.shuffle();
+  print(deck.toString());
 }
-
 class Deck {
-
   // An array of cards
-  List<Card> cards;
+  List<Card> cards = <Card>[];
+
+  // Constructor
+  Deck() {
+    // list of ranks
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spads'];
+
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(rank, suit);
+        cards.add(card);
+      }
+    }
+  }
+
+  @override
+  String toString() {
+    return cards.toString();
+  }
+
+  shuffle() {
+    cards.shuffle();
+  }
 }
 
 class Card {
   String suit;
   String rank;
+  Card(this.rank, this.suit);
+  @override
+  String toString() {
+    return '$rank of $suit';
+  }
 }
