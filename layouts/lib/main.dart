09e30611ae3eb3d06fc313bb40095067189f28d1
Baraka Mahili <georgesmahili@gmail.com>
2019-05-19 09:57:46 -0400

    Created a full card

--- layouts/lib/main.dart
+++ layouts/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 //https://www.youtube.com/watch?v=u0e2L5yoxFI
 void main() => runApp(MyApp());
 
@@ -26,7 +27,8 @@ class MyApp extends StatelessWidget {
             //       Radius.circular(5.0) //         <--- border radius here
             //       ),
             // ),
-            child: Demo(),
+            // child: Demo(),
+            child: DemoCard(),
           ),
         ));
   }
@@ -166,30 +168,83 @@ class Square extends StatelessWidget {
 // }
 
 // Using Stack: Items are stack up against each other...like position in css
-class Demo extends StatelessWidget {
+// class Demo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: MediaQuery.of(context).size.height, // device height
+//       width: MediaQuery.of(context).size.width, // device width
+//       // alignment: Alignment.center,
+//       child: Stack(
+//         children: <Widget>[
+//           Square(),
+//           Positioned(
+//             top: 20,
+//             left: 20,
+//             child: Square(
+//               color: Colors.blue,
+//             ),
+//           ),
+//           Positioned(
+//             top: 50,
+//             left: 50,
+//             child: Square(
+//               color: Colors.red,
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// Full Example
+
+class DemoCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height, // device height
-      width: MediaQuery.of(context).size.width, // device width
-      // alignment: Alignment.center,
-      child: Stack(
+      margin: EdgeInsets.all(24),
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+          border: Border.all(width: 1, color: Colors.black26),
+          borderRadius: BorderRadius.all(Radius.circular(6)),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 6,
+            )
+          ]),
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Square(),
-          Positioned(
-            top: 20,
-            left: 20,
-            child: Square(
-              color: Colors.blue,
-            ),
+          Container(
+            height: 200,
+            color: Colors.red,
+            child: Icon(Icons.ac_unit),
           ),
-          Positioned(
-            top: 50,
-            left: 50,
-            child: Square(
-              color: Colors.red,
-            ),
+          Text(
+            'Baraka Card 1',
+            style: TextStyle(fontSize: 25),
           ),
+          Text(
+            'This is a card created from scratch and you can tell how it scales up',
+            style: TextStyle(fontSize: 16),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 32),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                FlatButton(child: Text('Click Me!',),color: Colors.red,onPressed: (){},),
+                FlatButton(child: Text('Click Me!',),color: Colors.green,onPressed: (){},),
+                FlatButton(child: Text('Click Me!',),color: Colors.blue,onPressed: (){},),
+              ],
+            ),
+          )
         ],
       ),
     );
